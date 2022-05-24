b63bb57723e2452178a3ce1a07cad345f0c5e3af
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-05-24 13:24:12 -0500

    changing the problem

--- simple_optimization.py
+++ simple_optimization.py
@@ -8,6 +8,7 @@ y = cp.Variable()
 constraints = [x + y == 1,
                x - y >= 1]
 
+
 # Form objective.
 obj = cp.Minimize((x - y)**2)
 
@@ -17,3 +18,12 @@ prob.solve()  # Returns the optimal value.
 print("status:", prob.status)
 print("optimal value", prob.value)
 print("optimal var", x.value, y.value)
+
+# Replace the objective.
+prob2 = cp.Problem(cp.Maximize(x + y), prob.constraints)
+print("optimal value", prob2.solve())
+
+# Replace the constraint (x + y == 1).
+constraints = [x + y <= 3] + prob2.constraints[1:]
+prob3 = cp.Problem(prob2.objective, constraints)
+print("optimal value", prob3.solve())
